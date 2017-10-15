692676c14132d94fe7757ff68a4415f5c9a1c6cb
sungjunyoung <wnsdud1861@gmail.com>
2017-10-15 16:19:22 +0900

    init

---
+++ get-dataset.sh
@@ -0,0 +1,5 @@
+wget https://s3.ap-northeast-2.amazonaws.com/sungjunyoung-repository/maestro8/refined_category_dataset.img_feature.dat
+wget https://s3.ap-northeast-2.amazonaws.com/sungjunyoung-repository/maestro8/refined_category_dataset.img_feature.eval.dat
+wget https://s3.ap-northeast-2.amazonaws.com/sungjunyoung-repository/maestro8/soma8_eval_data.dat
+wget https://s3.ap-northeast-2.amazonaws.com/sungjunyoung-repository/maestro8/soma8_test_data.dat
+wget https://s3.ap-northeast-2.amazonaws.com/sungjunyoung-repository/maestro8/y_name_id_dict.dat
