644f562803b61ccb4ddd37e90fb7292171d977a7
sungjunyoung <wnsdud1861@gmail.com>
2017-10-15 16:31:51 +0900

    refined_category_dataset.dat 다운로드 포함

--- get-dataset.sh
+++ get-dataset.sh
@@ -1,3 +1,4 @@
+wget https://s3.ap-northeast-2.amazonaws.com/sungjunyoung-repository/maestro8/refined_category_dataset.dat
 wget https://s3.ap-northeast-2.amazonaws.com/sungjunyoung-repository/maestro8/refined_category_dataset.img_feature.dat
 wget https://s3.ap-northeast-2.amazonaws.com/sungjunyoung-repository/maestro8/refined_category_dataset.img_feature.eval.dat
 wget https://s3.ap-northeast-2.amazonaws.com/sungjunyoung-repository/maestro8/soma8_eval_data.dat
